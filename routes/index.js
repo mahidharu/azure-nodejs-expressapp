286bea2c9ad7144a42c041840b03e1c6372230c7
Mahidhar <43684464+mahidharu@users.noreply.github.com>
2021-07-22 22:38:18 -0500

    Adding logs

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("Inside home");
   res.render('index', { title: 'Welcome to Express App on Azure' });
 });
 
 /* Get Latest Date */
 router.get('/date', function(req, res, next) {
+  console.log("Inside date");
   res.send(new Date()).status(200);
 });
 
