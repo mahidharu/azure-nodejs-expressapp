6376b770ac39381269b5216cb690150fdc6a546b
Mahidhar <43684464+mahidharu@users.noreply.github.com>
2021-07-22 19:38:28 -0500

    Adding new route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to Express App on Azure' });
+});
+
+/* Get Latest Date */
+router.get('/date', function(req, res, next) {
+  res.send(new Date()).status(200);
 });
 
 module.exports = router;
